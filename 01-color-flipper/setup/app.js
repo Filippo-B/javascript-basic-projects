fbf7d1656b8b43830ffdb36253510646e4521c55
Filippo Bristot <filippo.bristot@hotmail.it>
2021-12-13 06:57:00 +0100

    feat(01): basic solution

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,24 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const colorEl = document.getElementsByClassName("color")[0];
+const body = document.getElementsByTagName("body")[0];
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * max + min);
+}
+
+function randomColorGenerator() {
+  return colors[randomInteger(0, colors.length - 1)];
+}
+
+function updateColor() {
+  const randomColor = randomColorGenerator();
+  colorEl.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+  return;
+}
+
+btn.addEventListener("click", function () {
+  updateColor();
+});
