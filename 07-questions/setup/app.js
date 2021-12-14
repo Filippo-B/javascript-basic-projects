b393377bf4a433e4a035d765c523573d4c6fb750
Filippo Bristot <filippo.bristot@hotmail.it>
2021-12-14 07:27:59 +0100

    feat(07): project completed

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,19 @@
 //using selectors inside the element
 // traversing the dom
+
+const questionBtn = document.getElementsByClassName("question-btn");
+const question = document.getElementsByClassName("question");
+
+Array.from(questionBtn).forEach((el) => {
+  const closestQuestion = el.closest(".question");
+  const removeShowText = () => Array.from(question).forEach((el) => el.classList.remove("show-text"));
+
+  el.addEventListener("click", function () {
+    if (!closestQuestion.classList.contains("show-text")) {
+      removeShowText();
+      closestQuestion.classList.add("show-text");
+    } else {
+      removeShowText();
+    }
+  });
+});
