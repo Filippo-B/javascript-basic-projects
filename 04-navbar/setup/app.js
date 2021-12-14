1b36e8e8e3a986fca897caaa6a2440e80fbb63a8
Filippo Bristot <filippo.bristot@hotmail.it>
2021-12-14 06:24:18 +0100

    feat(04): project completed

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,10 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.getElementsByClassName("nav-toggle")[0];
+const links = document.getElementsByClassName("links")[0];
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
