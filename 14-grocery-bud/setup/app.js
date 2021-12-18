af025b72f3f121efabdbe0739e466aeb10895aa6
Filippo Bristot <filippo.bristot@hotmail.it>
2021-12-18 08:02:23 +0100

    feat(14): add edit functionality

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -5,7 +5,9 @@ const submitBtn = document.getElementsByClassName("submit-btn")[0];
 const input = document.getElementById("grocery");
 const groceryContainer = document.getElementsByClassName("grocery-container")[0];
 const groceryList = document.querySelector(".grocery-list");
+const clearBtn = document.getElementsByClassName("clear-btn")[0];
 const deleteBtns = () => document.querySelectorAll(".delete-btn");
+const editBtns = () => document.querySelectorAll(".edit-btn");
 
 /**
  * iterare gli elementi dell'array
@@ -21,9 +23,16 @@ const deleteBtns = () => document.querySelectorAll(".delete-btn");
 const inputCurrentValue = () => new String(input.value).replace(/<(.*?)>/g, "&lt;$1&gt;").trim();
 const groceryListArr = [];
 
+function thisId(listItem) {
+  return listItem.closest(".grocery-item").getAttribute("data-item");
+}
+
 /* ============================================ */
 /* ··········································· § GROCERY LIST ··· */
 /* ======================================== */
+function clearInput() {
+  input.value = "";
+}
 function listItem(item) {
   return `<article class="grocery-item" data-item="${item}">
           <p class="title">${item}</p>
@@ -44,6 +53,22 @@ function groceryListOutput() {
   }
 }
 
+function deleteListItem(listItemToDelete) {
+  thisIndex = groceryListArr.findIndex((el) => el === thisId(listItemToDelete));
+  groceryListArr.splice(thisIndex, 1);
+}
+
+function setButtonValue(value) {
+  submitBtn.textContent = value;
+  submitBtn.setAttribute("data-action", value);
+}
+
+function editListItem(listItemToEdit) {
+  input.focus();
+  input.value = thisId(listItemToEdit);
+  listItemToEdit.setAttribute("data-action", "edit");
+  setButtonValue("edit");
+}
 /* =================================== § CHECK VISIBILITY CONTAINER === */
 function groceryContainerVisibility() {
   if (groceryListArr.length > 0) {
@@ -53,21 +78,15 @@ function groceryContainerVisibility() {
   }
 }
 
-function listItemsEventListeners() {
-  deleteBtns().forEach((deleteBtn) => {
-    deleteBtn.addEventListener("click", function () {
-      thisId = deleteBtn.closest(".grocery-item").getAttribute("data-item");
-      thisIndex = groceryListArr.findIndex((el) => el === thisId);
-      groceryListArr.splice(thisIndex, 1);
-      update();
-    });
-  });
-}
-
 function update() {
+  // check if the container of the list items should be visible
   groceryContainerVisibility();
+  // output the HTML for the list based on the element present in the array
   groceryListOutput();
+  // add the event listeners to the buttons of the newly created list items
   listItemsEventListeners();
+  // clear input content
+  clearInput();
 }
 
 /* ============================================ */
@@ -75,6 +94,35 @@ function update() {
 /* ======================================== */
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  groceryListArr.push(inputCurrentValue());
+  if (submitBtn.dataset.action === "submit") {
+    groceryListArr.push(inputCurrentValue());
+  } else if (submitBtn.dataset.action === "edit") {
+    setButtonValue("submit");
+    const currentlyEditingId = thisId(document.querySelector('[data-action="edit"]'));
+    const currentlyEditingIndex = groceryListArr.findIndex((el) => el === currentlyEditingId);
+    console.log(currentlyEditingIndex);
+    groceryListArr.splice(currentlyEditingIndex, 1, input.value);
+  }
   update();
 });
+
+clearBtn.addEventListener("click", function () {
+  groceryListArr.splice(0, groceryListArr.length);
+  update();
+});
+
+function listItemsEventListeners() {
+  deleteBtns().forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", function () {
+      deleteListItem(deleteBtn);
+      update();
+    });
+  });
+
+  editBtns().forEach((editBtn) => {
+    editBtn.addEventListener("click", function () {
+      editListItem(editBtn);
+      // update();
+    });
+  });
+}
